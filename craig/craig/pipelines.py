9ffd8416fca87c3ca674b77063c9e953730ef740
David <davidcondrey@me.com>
2016-05-19 03:41:15 -0700

    Update pipelines.py

--- craig/craig/pipelines.py
+++ craig/craig/pipelines.py
@@ -3,6 +3,6 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
-class CraigdatedPipeline(object):
+class CraigPipeline(object):
     def process_item(self, item, spider):
         return item
